65efcbee35e9a57b54481ee19ac6b882697c4ad1
josemibnf <josemi.bnf@outlook.es>
2020-07-03 19:04:34 +0200

    ..

--- start.py
+++ start.py
@@ -1,28 +1,6 @@
-from flask import Flask, request
 import gotzilla
 
-app = Flask(__name__)
+gateway = None
+refresh = None
 
-if __name__ == "__main__":
-    
-    # Una sesion por instancia.
-    
-    @app.route('/start', methods=['GET', 'POST'])
-    def start():
-        global Session 
-        gateway=request.json.get('gateway')
-        Session = gotzilla.new_session(gateway)
-        return {'auth':Session.get_auth()}
-
-    @app.route('/stop', methods=['POST'])
-    def stop():
-        global Session
-        auth = request.json.get('auth')
-        if auth == Session.get_auth() :
-            Session.stop()
-            
-    @app.route('/add/<image>', methods=['POST'])
-    def add(image):
-        gotzilla.add_solver(image)
-
-    app.run(host='0.0.0.0', port=8000)
+gotzilla.start(gateway, refresh)
