47f6de313da237d01102dc3b9ab7b8bfd01a09ad
josemibnf <josemi.bnf@outlook.es>
2020-09-17 21:18:26 +0200

    select

--- select.py
+++ select.py
@@ -1,9 +1,3 @@
-from flask import Flask, request
-import train
-
-app = Flask(__name__)
-
-
 def select(cnf):
     solvers = open('solvers.json', 'r')
     best_interpretation, best_interpretation_score = None, 0
@@ -11,14 +5,3 @@ def select(cnf):
         if cnf in solver and best_interpretation_score<solver[cnf]['score']:
             best_interpretation, best_interpretation_score = solver, solver[cnf]['score']
     return best_interpretation
-
-
-if __name__ == "__main__":
-
-    @app.route('/', methods=['GET', 'POST'])
-    def post():
-        cnf = request.json.get('cnf')
-        solution = select(cnf=cnf)
-        return {'interpretation':solution}
-
-    app.run(host='0.0.0.0', port=8080)
--- start.py
+++ start.py
@@ -1,10 +1,21 @@
-import gotzilla
 import sys
 import os
 
+from flask import Flask, request
+import train, select
+
+app = Flask(__name__)
+
+
 if __name__ == "__main__":
 
-    gateway = os.environ['GATEWAY']
-    refresh = os.environ['REFRESH']
+    @app.route('/', methods=['GET', 'POST'])
+    def post():
+        cnf = request.json.get('cnf')
+        solution = select.select(cnf=cnf)
+        return {'interpretation':solution}
 
-    gotzilla.start(gateway, refresh)
+    def get():
+        train.start( os.environ['GATEWAY'], os.environ['REFRESH'])
+
+    app.run(host='0.0.0.0', port=8080)
