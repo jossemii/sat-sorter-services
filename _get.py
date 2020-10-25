c372da3144d119ec139e3c83e76ef65df9f530f7
josemibnf <josemi.bnf@outlook.es>
2020-10-25 14:04:20 +0100

    select updated.

--- _get.py
+++ _get.py
@@ -1,7 +1,6 @@
 def cnf(cnf):
-    solvers = open('solvers.json', 'r')
-    best_interpretation, best_interpretation_score = None, 0
-    for solver in solvers:
-        if cnf in solver and best_interpretation_score<solver[cnf]['score']:
-            best_interpretation, best_interpretation_score = solver, solver[cnf]['score']
-    return best_interpretation
+    with open('/satrainer/solvers.json', 'r',  best_interpretation=None, best_interpretation_score=0) as solvers:
+        for solver in solvers:
+            if cnf in solver and best_interpretation_score<solver[cnf]['score']:
+                best_interpretation, best_interpretation_score = solver, solver[cnf]['score']
+        return best_interpretation
