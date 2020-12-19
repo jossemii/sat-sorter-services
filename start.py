85ce2d999b1166122f76dceb9e050c7d7788ae7b
josemibnf <josemi.bnf@outlook.es>
2020-12-19 16:45:47 +0100

    start upload reimplemented with the correct way.

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 from flask import Flask, request
 import train, _get
 
@@ -15,15 +15,17 @@ if __name__ == "__main__":
 
     @app.route('/upsolver', methods=['GET', 'POST'])
     def up_solver():
-        with open('/satrainer/solvers.json', 'w') as solvers:
-            solvers.update({
-                request.get_json()['solver']: {}
-            })
+        data = json.load('/satrainer/solvers.json', 'r')
+        data.update({
+            request.get_json()['solver']: {}
+        })
+        with open('/satrainer/solvers.json', 'w') as file:
+            json.dump(data, file)
         return 'DoIt'
 
     @app.route('/train/start', methods=['GET'])
     def start_train():
-        train.Session.__call__().init( os.environ['GATEWAY'] ) # subprocess
+        train.Session.__call__().init(os.environ['GATEWAY']) # subprocess
         # return 'DoIt'
 
     @app.route('/train/stop', methods=['GET'])
