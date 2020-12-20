ee17e931eb8bfac124933cbab1e96831bc98d142
josemibnf <josemi.bnf@outlook.es>
2020-12-20 21:35:04 +0100

    regression takes the best degree function of each solver

--- regresion.py
+++ regresion.py
@@ -1,14 +1,24 @@
 from time import sleep
-
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
 import json
 DIR = '' # /satrainer/
+MAX_DEGREE = 100
 
-def solver_regresion(solver: dict, coffdet: tuple):
 
+def regression_with_degree(degree: int, input: np.array, output: np.array):
+    input = PolynomialFeatures(degree= degree, include_bias=False).fit_transform(input)
+
+    # Create a model of regression.
+    model = LinearRegression().fit(input, output )
+    return {
+        'tensor params': model.intercept_.tolist()+model.coef_[0].tolist(),
+        'coefficient of determination': model.score(input, output)
+    }
+
+def solver_regression(solver: dict):
     # Get input variables. Num of cnf variables and Num of cnf clauses.
     input = np.array(
         [[int(var) for var in value.split(':')] for value in solver]
@@ -19,27 +29,37 @@ def solver_regresion(solver: dict, coffdet: tuple):
         [value.get('score') or None for value in solver.values()]
     ).reshape(-1, 1)
 
-    # Utility to transform the input into a 'degree' grade function.
-    input = PolynomialFeatures(degree=2, include_bias=True).fit_transform(input)
+    best_tensor = {'coefficient of determination': 0}
+    for degree in range(1, MAX_DEGREE+1):
+        tensor = regression_with_degree(degree= degree, input=input, output=output)
+        if tensor['coefficient of determination'] > best_tensor['coefficient of determination']:
+            best_tensor = tensor
+    return best_tensor
 
-    # Create a model of regression.
-    while r2 not in range(coffdet[0], coffdet[1]):
-        model = LinearRegression(fit_intercept=False).fit(input, output)
-        r2 = model.score() # coefficient of determination
-
-    return [model.intercept_]+model.coef_
-
-def iterate_regresion(coffdet: tuple):
+def iterate_regression():
     # Read solvers.json
     with open(DIR + 'solvers.json', 'r') as file:
         solvers = json.load(file)
 
+    tensors = {}
+
     # Make regression for each solver.
     for solver in solvers:
-        solver_regresion(solver=solvers[solver], coffdet=coffdet)
+        if solvers[solver]=={}: continue
+        tensor = solver_regression(solver=solvers[solver])
+
+        print('SOLVER --> ', solver)
+        print('R2 --> ', tensor['coefficient of determination'])
+        print('TENSOR --> ', tensor['tensor params'])
+        print(' ------ ')
+
+        tensors.update({solver: tensor['tensor params']})
+
+    # Write tensors.json
+    with open((DIR)+'tensors.json', 'w') as file:
+        json.dump(tensors, file)
 
 if __name__ == "__main__":
     while True:
-        iterate_regresion((0.5, 0.8))
-        sleep(20)
-
+        iterate_regression()
+        sleep(9999)
