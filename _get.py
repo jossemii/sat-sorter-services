33adf6caf14afcc4681d353e89c7b2ac625b9139
josemibnf <josemi.bnf@outlook.es>
2020-12-24 20:53:45 +0100

    _get get core

--- _get.py
+++ _get.py
@@ -1,8 +1,19 @@
-def cnf(cnf):
-    with open('/satrainer/solvers.json', 'r') as solvers:
+def get_score(tensor: dict, cnf: dict):
+    return tensor['coefficient'] * (cnf['clauses']**tensor['c']) * (cnf['literals']**tensor['l'])
+
+def data(cnf: dict):
+    for clause in cnf:
+            for literal in clause:
+                if abs(literal) > num_literals:
+                    num_literals = abs(literal)
+    return {'clauses': len(cnf), 'literals': num_literals}
+
+def cnf(cnf: dict):
+    with open('/satrainer/tensors.json', 'r') as tensors:
         best_interpretation = None
         best_interpretation_score = 0
-        for solver in solvers:
-            if cnf in solver and best_interpretation_score<solver[cnf]['score']:
-                best_interpretation, best_interpretation_score = solver, solver[cnf]['score']
+        for tensor in tensors:
+            score = get_score(tensor=tensors[tensor], cnf=data(cnf))
+            if best_interpretation_score < score:
+                best_interpretation, best_interpretation_score = tensor, score
         return best_interpretation
