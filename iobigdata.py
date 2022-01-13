72a7668f5c6b393241e9b1e3874f20725bcaa017
jossemii <josemi.bnf@gmail.com>
2022-01-13 22:54:23 +0100

    io

--- iobigdata.py
+++ iobigdata.py
@@ -2,7 +2,22 @@
 import psutil, gc
 from time import sleep
 from threading import Lock
-from singleton import Singleton
+
+# Thread-sage Singleton
+import threading
+class Singleton(type):
+  _instances = {}
+  _lock = threading.Lock()
+
+  def __call__(cls, *args, **kwargs):
+    if cls not in cls._instances:
+      with cls._lock:
+        # another thread could have created the instance
+        # before we acquired the lock. So check that the
+        # instance is still nonexistent.
+        if cls not in cls._instances:
+          cls._instances[cls] = super().__call__(*args, **kwargs)
+    return cls._instances[cls]
 
 mem_manager = lambda len: IOBigData().lock(len=len)
 
