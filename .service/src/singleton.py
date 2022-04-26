ab458f347cda3ac175f1eea7d21f7e5a6c5e0f04
josemii <40121100+jossemii@users.noreply.github.com>
2022-04-26 18:26:57 +0200

    Delete .service/src directory

--- .service/src/singleton.py
+++
@@ -1,7 +0,0 @@
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, ENVS):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(ENVS)
-        return cls._instances[cls]
--- .service/src/utils.py
+++
@@ -1,6 +0,0 @@
-def read_file(filename) -> bytes:
-    def generator(filename):
-        with open(filename, 'rb') as entry:
-            for chunk in iter(lambda: entry.read(1024 * 1024), b''):
-                yield chunk
-    return b''.join([b for b in generator(filename)])
