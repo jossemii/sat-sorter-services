a7f88c27deb65198b1da274ec086684132374969
jossemii <jossemii@proton.me>
2024-01-14 19:33:54 +0100

    fixed random-cnf usage of grpcbb

--- random-cnf-generator/start.py
+++ random-cnf-generator/start.py
@@ -3,13 +3,14 @@ if __name__ == "__main__":
 
     import randomCNF
     from concurrent import futures
-    import grpc, grpcbigbuffer
+    import grpc
+    from grpcbigbuffer.client import serialize_to_buffer
     import api_pb2_grpc
 
 
     class RandomCnf(api_pb2_grpc.RandomServicer):
         def RandomCnf(self, request, context):
-            for b in grpcbigbuffer.serialize_to_buffer(
+            for b in serialize_to_buffer(
                     message_iterator=randomCNF.ok()
             ): yield b
 
