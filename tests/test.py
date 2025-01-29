7694b8e5d1b2a7e67ba288d79ac89af06faed590
0xf965 <thisemaildoesnotexists@anon.erg>
2025-01-29 20:58:19 +0100

    frontier updated.

--- tests/test.py
+++ tests/test.py
@@ -17,7 +17,7 @@ from node_controller.gateway.protos import gateway_pb2, celaut_pb2, gateway_pb2_
 GATEWAY="localhost:53047"
 CLIENT_DEV=""
 RANDOM="54500441c6e791d9f6ef74102f962f1de763c9284f17a8ffde3ada9026d55089"
-FRONTIER=""
+FRONTIER="900adcdd218c60a02d061fe9853c554be3c9b50616c085c10c404a7befdedf19"
 SORTER="098b5c01cd051ef759a54ba1a1d133bf2c196704df6fe5916e08460c9819a082"
 
 METADATA_REGISTRY="/nodo/storage/__metadata__"
